6ec0f0292ec01e8a99ca01d9bb6c02a1bf1da4b3
Brayan-chan <al071392@uacam.mx>
2025-04-26 15:18:19 -0600

    Agregar archivo de rutas de pago con ejemplos de uso

---
+++ public/routes/payment.routes.js
@@ -0,0 +1,23 @@
+// Importamos las dependencias necesarias
+import { Router } from "express";
+
+// Creamos la constante de la ruta
+const router = Router();
+
+/* Ejemplos de uso de la ruta
+router.get('/example', (req, res) => {
+    res.send('Example');
+});
+router.post('/example', (req, res) => {
+    res.send('Example');
+});
+router.put('/example', (req, res) => {
+    res.send('Example');
+});
+router.delete('/example', (req, res) => {
+    res.send('Example');
+});
+*/
+
+// Exportamos la constante
+export default router;
