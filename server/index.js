68ab757f8f02c6f4a65162268e302172a67ec2dc
Brayan-chan <al071392@uacam.mx>
2025-04-26 15:42:59 -0600

    feat: mejorar la configuración del servidor y agregar registro de solicitudes HTTP

--- server/index.js
+++ server/index.js
@@ -1,19 +1,29 @@
 // Importar el módulo Express
 import express from 'express';
+
+// Importamos Morgan
+import morgan from 'morgan';
+
+// Importar el módulo de configuración
+import {PORT} from './config.js';
+
 // Cambiar la ruta para que apunte al archivo correcto
 import paymentRoutes from './routes/payment.routes.js';
 
 // Crear una instancia de la aplicación Express
 const app = express();
 
+// Configurar Morgan para registrar las solicitudes HTTP
+app.use(morgan('dev'));
+
 // Servir archivos estáticos desde el directorio "public"
 app.use(express.static('public'));
 app.use(paymentRoutes);
 
 // Configurar el puerto en el que el servidor escuchará
-const PORT = 3000;
+const port = (PORT);
 
 // Iniciar el servidor y escuchar en el puerto especificado
 app.listen(PORT, () => {
-    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+    console.log(`Servidor ejecutándose en http://localhost:${port}`);
 });
