20e33b5962a1f254617fe6592a482e69a59c33ec
Brayan-chan <al071392@uacam.mx>
2025-05-05 02:18:56 -0600

    feat: eliminar variables de entorno y usar valores de configuración de Firebase directamente

--- public/assets/js/firebase-config.js
+++ public/assets/js/firebase-config.js
@@ -1,28 +1,28 @@
 // Importar Firebase y sus módulos
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-
-// Importamos dotenv para cargar las variables de entorno
-import dotenv from 'dotenv';
-
-// Cargamos las variables de entorno desde el archivo .env
-dotenv.config();
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js"
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js"
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js"
 
 // Configuración de Firebase
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-  };
+  apiKey: "AIzaSyA_5IiaWrQbw5CrRYtOpnYYQ2aUpDpMUKE",
+  authDomain: "carrito-compras-38f0f.firebaseapp.com",
+  projectId: "carrito-compras-38f0f",
+  storageBucket: "carrito-compras-38f0f.appspot.com",
+  messagingSenderId: "139215439065",
+  appId: "1:139215439065:web:7a7a588fcf7781ca3a6ac8",
+}
 
 // Inicializar Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const db = getFirestore(app)
+
+// Configurar para trabajar offline
+const firestoreSettings = {
+  cacheSizeBytes: 50000000, // 50 MB
+  ignoreUndefinedProperties: true,
+}
 
 // Exportar la configuración de Firebase
-export { app, auth, db };
+export { app, auth, db }
